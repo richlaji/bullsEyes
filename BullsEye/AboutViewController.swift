128163dbb0c5571e4bda6909d2cfa5e4049a8b58
张泽昕 <zhangzexin@zhangzexindeMacBook-Pro.local>
2016-02-10 20:03:53 +0800

    finish

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -9,18 +9,23 @@
 import UIKit
 
 class AboutViewController: UIViewController {
-
+    //variable
+    @IBOutlet weak var webView: UIWebView!
+    
+    //func
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        if let htmlFile = NSBundle.mainBundle().pathForResource("BullsEye",ofType: "html") {
+            if let htmlData=NSData(contentsOfFile: htmlFile) {
+                let baseURL = NSURL(fileURLWithPath: NSBundle.mainBundle().bundlePath)
+                webView.loadData(htmlData, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+            }
+        }
     }
     
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -7,19 +7,108 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
-
+    //variable
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    
+    //func
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        startNewGame()
+        updateLabels()
+        slider.setThumbImage(UIImage(named: "SliderThumb-Normal"), forState: .Normal)
+        slider.setThumbImage(UIImage(named: "SliderThumb-Highlighted"), forState: .Highlighted)
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        if let trackLeftImage = UIImage(named: "SliderTrackLeft") {
+            let trackLeftResizable = trackLeftImage.resizableImageWithCapInsets(insets)
+            slider.setMinimumTrackImage(trackLeftResizable, forState: .Normal)
+        }
+        if let trackRightImage = UIImage(named: "SliderTrackRight") {
+            let trackRightResizable = trackRightImage.resizableImageWithCapInsets(insets)
+            slider.setMaximumTrackImage(trackRightResizable, forState: .Normal)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func showAlert() {
+        //calculate
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        score += points
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+        } else if difference < 5 {
+            title = "You almost had it!"
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
+        
+        
+        //show alert
+        let message = "You scored \(points)"
+        let alert = UIAlertController(title: title,message: message,preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: {action in self.startNewRound(); self.updateLabels()})
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+        //update
+        //startNewRound()
+        //updateLabels()
+    }
+    
+    @IBAction func sliderMoved(slider: UISlider) {
+        //test
+        //print("The value of the slider is now: \(slider.value)")
+        //
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func startOver() {
+        startNewGame()
+        updateLabels()
+        
+        let transition = CATransition()
+        transition.type = kCATransitionFade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        view.layer.addAnimation(transition, forKey: nil)
+    }
+    
+    func startNewGame() {
+        score = 0
+        round = 0
+        startNewRound()
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        round += 1
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
 }
 
